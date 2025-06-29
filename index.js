ae74ae69941e8b6580a0bd37434291c771caa3c9
fredrickmwangi <fredmallin49@gmail.com>
2025-06-29 23:35:13 +0300

    initialize my project

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // declared without var, let, or const — becomes global
+}
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'; // overwrites global `bestCustomer`
+}
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else'; // will throw an error!
+}
